3bb29d51c972755a369f72dd86ea1a50327c6abd
Marc Eder <6818607+meder411@users.noreply.github.com>
2020-05-01 23:34:11 -0400

    jit example

--- cuda/jit.py
+++ cuda/jit.py
@@ -1,4 +1,6 @@
 from torch.utils.cpp_extension import load
 lltm_cuda = load(
-    'lltm_cuda', ['lltm_cuda.cpp', 'lltm_cuda_kernel.cu'], verbose=True)
+    'lltm_cuda', ['lltm_cuda.cpp', 'lltm_cuda_kernel.cu'],
+    extra_include_paths=['includes'],
+    verbose=True)
 help(lltm_cuda)
